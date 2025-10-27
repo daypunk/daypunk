9e79d753c146d7db1e85b311be1fc43bd769b9d3
daypunk <daehee3853@gmail.com>
2025-10-27 20:27:48 +0900

    Update weather API to OpenWeatherMap and adjust emoji mapping; modify chat.svg for current weather display

--- build-svg.js
+++ build-svg.js
@@ -4,39 +4,66 @@ const WEATHER_API_KEY = process.env.WEATHER_API_KEY
 
 import fs from 'fs'
 import got from 'got'
-import Qty from 'js-quantities/esm'
 
-let WEATHER_DOMAIN = 'http://dataservice.accuweather.com'
+let WEATHER_DOMAIN = 'https://api.openweathermap.org'
 
+// OpenWeatherMap weather condition codes to emoji mapping
 const emojis = {
-  1: '☀️',
-  2: '☀️',
-  3: '🌤',
-  4: '🌤',
-  5: '🌤',
-  6: '🌥',
-  7: '☁️',
-  8: '☁️',
-  11: '🌫',
-  12: '🌧',
-  13: '🌦',
-  14: '🌦',
-  15: '⛈',
-  16: '⛈',
-  17: '🌦',
-  18: '🌧',
-  19: '🌨',
-  20: '🌨',
-  21: '🌨',
-  22: '❄️',
-  23: '❄️',
-  24: '🌧',
-  25: '🌧',
-  26: '🌧',
-  29: '🌧',
-  30: '🥵',
-  31: '🥶',
-  32: '💨',
+  200: '⛈', // thunderstorm with light rain
+  201: '⛈', // thunderstorm with rain
+  202: '⛈', // thunderstorm with heavy rain
+  210: '⛈', // light thunderstorm
+  211: '⛈', // thunderstorm
+  212: '⛈', // heavy thunderstorm
+  221: '⛈', // ragged thunderstorm
+  230: '⛈', // thunderstorm with light drizzle
+  231: '⛈', // thunderstorm with drizzle
+  232: '⛈', // thunderstorm with heavy drizzle
+  300: '🌦', // light intensity drizzle
+  301: '🌦', // drizzle
+  302: '🌦', // heavy intensity drizzle
+  310: '🌦', // light intensity drizzle rain
+  311: '🌦', // drizzle rain
+  312: '🌦', // heavy intensity drizzle rain
+  313: '🌦', // shower rain and drizzle
+  314: '🌦', // heavy shower rain and drizzle
+  321: '🌦', // shower drizzle
+  500: '🌧', // light rain
+  501: '🌧', // moderate rain
+  502: '🌧', // heavy intensity rain
+  503: '🌧', // very heavy rain
+  504: '🌧', // extreme rain
+  511: '🌧', // freezing rain
+  520: '🌧', // light intensity shower rain
+  521: '🌧', // shower rain
+  522: '🌧', // heavy intensity shower rain
+  531: '🌧', // ragged shower rain
+  600: '🌨', // light snow
+  601: '🌨', // snow
+  602: '🌨', // heavy snow
+  611: '🌨', // sleet
+  612: '🌨', // light shower sleet
+  613: '🌨', // shower sleet
+  615: '🌨', // light rain and snow
+  616: '🌨', // rain and snow
+  620: '❄️', // light shower snow
+  621: '❄️', // shower snow
+  622: '❄️', // heavy shower snow
+  701: '🌫', // mist
+  711: '🌫', // smoke
+  721: '🌫', // haze
+  731: '💨', // sand/dust whirls
+  741: '🌫', // fog
+  751: '💨', // sand
+  761: '💨', // dust
+  762: '🌫', // volcanic ash
+  771: '💨', // squalls
+  781: '🌪', // tornado
+  800: '☀️', // clear sky
+  801: '🌤', // few clouds
+  802: '🌥', // scattered clouds
+  803: '🌥', // broken clouds
+  804: '☁️', // overcast clouds
 }
 
 
@@ -49,16 +76,14 @@ const todayDay = new Intl.DateTimeFormat('en-US', {
 }).format(today)
 
 // 서울 실시간 날씨 정보
-const locationKey = '226081'
-let url = `currentconditions/v1/${locationKey}?apikey=${WEATHER_API_KEY}`
+let url = `data/2.5/weather?q=Seoul,KR&appid=${WEATHER_API_KEY}&units=metric`
 
 got(url, { prefixUrl: WEATHER_DOMAIN })
   .then((response) => {
     let json = JSON.parse(response.body)
 
-    const degF = Math.round(json[0].Temperature.Imperial.Value)
-    const degC = Math.round(Qty(`${degF} tempF`).to('tempC').scalar)
-    const icon = json[0].WeatherIcon
+    const degC = Math.round(json.main.temp)
+    const weatherCode = json.weather[0].id
 
     fs.readFile('template.svg', 'utf-8', (error, data) => {
       if (error) {
@@ -66,7 +91,7 @@ got(url, { prefixUrl: WEATHER_DOMAIN })
       }
 
       data = data.replace('{degC}', degC)
-      data = data.replace('{weatherEmoji}', emojis[icon] || '🌤️')
+      data = data.replace('{weatherEmoji}', emojis[weatherCode] || '🌤️')
       data = data.replace('{todayDay}', todayDay)
 
       data = fs.writeFile('chat.svg', data, (err) => {
